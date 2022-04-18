ad300d49308a702fe7326cc94605d8d0a9f94d6f
Aditya Ghidora <adityaghidora@gmail.com>
2022-04-19 01:04:13 +0530

    learned about buttons & got one to flip b/w strings

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,10 +4,36 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+  void CheckAnswer() {
+    setState(() => {questionIndex = (questionIndex + 1) % 2});
+  }
+
+  List<String> questions = [
+    "Who are thou?",
+    "What is thy name?",
+  ];
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("hello!"),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: Text("This is the app bar title")),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(child: Text("Click me!"), onPressed: CheckAnswer),
+          ],
+        ),
+      ),
     );
   }
 }
