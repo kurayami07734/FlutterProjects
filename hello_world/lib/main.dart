d6bb2a589bee66f3eca9e78e3b1b383a0c285da7
Aditya Ghidora <adityaghidora@gmail.com>
2022-04-30 16:53:44 +0530

    completed i am poor assignment

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.blueGrey[200],
-          appBar: AppBar(
-            title: Text('Diamond no Jundo'),
-            backgroundColor: Colors.blueGrey[900],
+        backgroundColor: Colors.blueGrey[200],
+        appBar: AppBar(
+          title: Text('Diamond no Jundo'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/sky.jpeg'),
           ),
-          body: Center(
-              child: Image(
-            image: NetworkImage(
-                'https://static.wikia.nocookie.net/yahari/images/a/a1/Diamond_no_Jundo_Cover_1.jpg/revision/latest?cb=20200927023455'),
-          )))));
+        ),
+      ),
+    ),
+  );
 }
